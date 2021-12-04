d1deec4835bbb8eebc41d8693347f549aeaac6dc
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-04 17:13:12 -0300

    feat(requisiçoes funcionando): preparando terreno para services

--- src/routes.ts
+++ src/routes.ts
@@ -1,1 +1,18 @@
-// import { Router } from 'express'
+import { Router } from 'express'
+import { CreateEncriptController } from './controllers/CreateEncriptController'
+
+const router = Router()
+
+const createEncriptController = new CreateEncriptController()
+
+router.post(
+  '/encript',
+  createEncriptController.encript
+)
+
+router.get(
+  '/encript/:id',
+  createEncriptController.decrypt
+)
+
+export { router }
