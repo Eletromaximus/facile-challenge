cd45b5510e53d238f80aba898ba7e4e2c1846813
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-05 18:50:27 -0300

    feat(adicionando cors): adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import 'express-async-errors'
 import { router } from './routes'
+import cors from 'cors'
 import handleError from './utils/HandleError'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use(router)
 
