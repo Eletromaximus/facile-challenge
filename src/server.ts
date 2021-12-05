17914e483116005ac4a1e2e56d574302c9984b6d
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-05 20:03:44 -0300

    feat(adicionando url encoded extendida): adicionanda mudança na porta

--- src/server.ts
+++ src/server.ts
@@ -9,9 +9,9 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
-
+app.use(express.urlencoded({ extended: true }))
 app.use(router)
 
 app.use(handleError)
 
-app.listen(process.env.PORT, () => console.log('welcome to the server'))
+app.listen(process.env.PORT || 5000, () => console.log('welcome to the server'))
