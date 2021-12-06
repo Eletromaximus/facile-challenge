e093e92e64e570300644fc3fdd853046e54edc62
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-12-06 08:40:41 -0300

    fix(finalizado): erro nas variaveis de ambiente

--- src/controllers/CreateEncryptController.ts
+++ src/controllers/CreateEncryptController.ts
@@ -6,23 +6,19 @@ export class CreateEncryptController {
   async encrypt (req: Request, res: Response) {
     const data = req.body
 
-    if (data.message) {
-      const { message } = data
-      try {
-        const createEncryptService = new CreateEncryptService()
+    const { message } = data
+    try {
+      const createEncryptService = new CreateEncryptService()
 
-        const encript = await createEncryptService.execute(message.toString())
+      const encript = await createEncryptService.execute(message.toString())
 
-        res.json(encript)
-      } catch (error: any) {
-        if (error instanceof CustomError) {
-          res.status(error.status).json(error.message)
-        } else {
-          res.status(error.status).json('Erro interno')
-        }
+      return res.json(encript)
+    } catch (error: any) {
+      if (error instanceof CustomError) {
+        res.status(error.status).json(error.message)
+      } else {
+        res.status(error.status).json('Erro interno')
       }
-    } else {
-      res.status(400)
     }
   }
 }
